6cee7c67bd584caa3a105757a9f49890080056c1
joaoguzman <joaogg@ug.uchile.cl>
2020-11-18 11:12:30 -0300

    estructura inicial

---
+++ servicios_financieros.py
@@ -0,0 +1,30 @@
+class Cliente:
+    def __init__(self, nombre, id, saldo):
+        self.nombre=nombre
+        self.id=id
+        self.saldo=saldo
+    def girar(self):
+    
+    def abonar(self):
+        
+    def mostrar_saldo(self):
+        
+
+class Financiera:
+    def __init__(self, nombre, id, saldo_institucional, clientes):
+        self.nombre=nombre
+        self.id=id
+        self.saldo_institucional=saldo_institucional
+        self.clientes=clientes
+    def agregar_cliente(self):
+        
+    def eliminar_cliente(self):
+        
+    def transferir(self):
+        
+    def giros_totles(self):
+    
+    def abonos_totales(self):
+        
+    def mostrar_saldo_institucional(self):
+        
