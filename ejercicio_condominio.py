7ccd3116fae9a369f02b574e146a3dadf2e14dde
Joao Guzmán <joaogg@ug.uchie.cl>
2020-11-25 12:22:50 -0300

    estructura basica ejercicio condominio

---
+++ ejercicio_condominio.py
@@ -0,0 +1,71 @@
+class Condominio:
+    def __init__(self, direccion, lista_administrador, lista_guardias, num_unidades_habitacionales, lista_unidades, cuenta_corriente, atributo1, atributo2, atributo3, atributo4)
+        self.direccion = direccion
+        self.lista_administrador = administrador
+        self.lista_guardias = lista_guardias
+        self.num_unidades_habitacionales = num_unidades_habitacionales
+        self.lista_unidades = lista_unidades
+        self.cuenta_corriente = cuenta_corriente
+        self.atributo1 = atributo1
+        self.atributo2 = atributo2
+        self.atributo3 = atributo3
+        self.atributo4 = atributo4
+    def get_direccion(self):
+        pass
+    def set_direccion(self):
+        pass
+    def set_administrador(self):
+        pass
+    def get_administrador(self):
+        pass
+    def add_guardia(self):
+        pass
+    def del_guardia(self):
+        pass
+    def get_guardias(self):
+        pass
+    def get_unidades(self):
+        pass
+    def metodo1(self):
+        pass
+    def metodo2(self):
+        pass
+    def metodo3(self):
+        pass
+    def metodo4(self):
+        pass
+#Creación de clases Guardia, UnidadHabitacional, CuentaCorriente con 3 atributos y 4 métodos.
+class Guardia:
+    def __init__(self, atributo1, atributo2, atributo3):
+        pass
+    def metodo1(self):
+        pass
+    def metodo2(self):
+        pass
+    def metodo3(self):
+        pass
+    def metodo4(self):
+        pass
+class UnidadHabitacional:
+    def __init__(self):
+        pass
+    def metodo1(self):
+        pass
+    def metodo2(self):
+        pass
+    def metodo3(self):
+        pass
+    def metodo4(self):
+        pass
+class CuentaCorriente:
+    def __init__(self):
+        pass
+    def metodo1(self):
+        pass
+    def metodo2(self):
+        pass
+    def metodo3(self):
+        pass
+    def metodo4(self):
+        pass
+
